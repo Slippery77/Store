398ebe049c4d2ace9dd297aeb4cd7efcd99f4be8
Slippery77 <103355666+Slippery77@users.noreply.github.com>
2024-09-24 22:53:14 +0700

    Create Milk.java

---
+++ Store/Milk.java
@@ -0,0 +1,10 @@
+public class Milk extends Product{
+    private int volumn;
+    public Milk(double p){
+        super("Milk",p);
+    }
+    public void setVolumn(int v){
+        super.setVolumn(v);
+    } 
+    
+}
