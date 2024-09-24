434d82b840310743b57cde6144af566e95bc64c4
Slippery77 <103355666+Slippery77@users.noreply.github.com>
2024-09-24 22:53:46 +0700

    Create Coffee.java

---
+++ Store/Coffee.java
@@ -0,0 +1,10 @@
+public class Coffee extends Product {
+    public Coffee(double price){
+        super("Coffee",price);
+    }
+    @Override
+    public void setWeight(int weight) {
+        // TODO Auto-generated method stub
+        super.setWeight(weight);
+    }
+}
